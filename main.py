404dfec99ba06297a6465ebdc63c3f4ada7e7446
ktap23k <ktap23k@gmail.com>
2024-12-07 14:42:13 +0700

    add web

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import items
+from app.services.item_service import read_led_config
 
 app = FastAPI()
 
 app.include_router(items.router, prefix="/items", tags=["items"])
 
-# @app.get("/")
-# def read_root():
-#     return {"message": "Welcome to FastAPI app!"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://led.mycode.vn", 
+        "https://led.mycode.vn",
+    ],  # Thay "*" bằng một danh sách cụ thể các origin được phép
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/statics"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    config = read_led_config()
+    return templates.TemplateResponse("index.html", {"request": request, "config": config})
 
 # @app.get("/list-routers")
 # def list_routers():
