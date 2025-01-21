00841d3a8ac605087b3d6530fac7c4b609dd1e5f
ktap23k <ktap23k@gmail.com>
2025-01-21 18:35:13 +0700

    add

--- app/api/endpoints/items.py
+++ app/api/endpoints/items.py
@@ -1,7 +1,12 @@
 import os
 from fastapi import APIRouter, Depends, HTTPException, status, Header
 from app.schemas.item import Item
-from app.services.item_service import read_wifi_config, write_wifi_config, read_led_config, write_led_config
+from app.services.item_service import (
+    read_wifi_config,
+    write_wifi_config,
+    read_led_config,
+    write_led_config,
+)
 
 router = APIRouter()
 
@@ -9,48 +14,45 @@ router = APIRouter()
 def verify_api_key(x_api_key: str = Header(None)):
     # Get the API key from environment variables
     APIKEY = os.getenv("API_KEYS")
+    print(APIKEY, x_api_key)
 
     if x_api_key != APIKEY:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid API Key"
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API Key"
         )
+
 
 @router.get("/led/wifi")
 def get_items(api_key: str = Depends(verify_api_key)):
     return read_wifi_config()
+
 
 @router.post("/led/wifi/data")
 def add_item(item: Item, api_key: str = Depends(verify_api_key)):
     write_wifi_config(item.dict())
     return item
 
+
 @router.post("/led/wifi/{ssid}/{password}")
 def add_item(ssid: str, password: str, api_key: str = Depends(verify_api_key)):
-    data = {
-        "ssid": ssid,
-        "password": password
-    }
+    data = {"ssid": ssid, "password": password}
     write_wifi_config(data)
     return data
 
+
 @router.post("/led/config/on/{seed}")
 def get_items(seed: int, api_key: str = Depends(verify_api_key)):
-    data = {
-        "led": 1,
-        "seed": seed
-    }
+    data = {"led": 1, "seed": seed}
     write_led_config(data)
     return data
 
+
 @router.post("/led/off")
 def get_items(api_key: str = Depends(verify_api_key)):
-    data = {
-        "led": 0,
-        "seed": 0
-    }
+    data = {"led": 0, "seed": 0}
     write_led_config(data)
     return data
+
 
 @router.get("/led/status")
 def get_items(api_key: str = Depends(verify_api_key)):
--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import items
-from app.services.item_service import read_led_config
+from app.services.item_service import read_led_config, write_led_config
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+import datetime
+import pytz
 
 app = FastAPI()
 
@@ -13,7 +17,7 @@ app.include_router(items.router, prefix="/items", tags=["items"])
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://led.mycode.vn", 
+        "http://led.mycode.vn",
         "https://led.mycode.vn",
     ],  # Thay "*" bằng một danh sách cụ thể các origin được phép
     allow_credentials=True,
@@ -24,18 +28,54 @@ app.add_middleware(
 templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory="app/statics"), name="static")
 
+timezone = pytz.timezone("Asia/Bangkok")  # UTC+7
+
+
+def scheduled_task():
+    print(f"Task executed at {datetime.datetime.now()}")
+    # check if time is 6h, 12h, 18h UTC+7
+    if datetime.datetime.now(timezone).hour == 6:
+        data = {"led": 1, "seed": 40000}
+    if datetime.datetime.now(timezone).hour == 12:
+        data = {"led": 1, "seed": 60000}
+    if datetime.datetime.now(timezone).hour == 18:
+        data = {"led": 0, "seed": 0}
+    write_led_config(data)
+
+
+scheduler = BackgroundScheduler()
+
+# Thêm job vào scheduler (chạy lúc 6h, 12h, 18h mỗi ngày)
+trigger = CronTrigger(
+    hour="6,12,18", minute=0, timezone=timezone
+)  # Giờ: 6h, 12h, 18h, phút: 0
+scheduler.add_job(scheduled_task, trigger=trigger)
+
+scheduler.start()
+
+
+@app.on_event("shutdown")
+def shutdown_event():
+    scheduler.shutdown()
+
+
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
+
 @app.get("/led", response_class=HTMLResponse)
 def read_root(request: Request):
     config = read_led_config()
-    return templates.TemplateResponse("index.html", {"request": request, "config": config})
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "config": config}
+    )
+
 
 @app.get("/logout", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("logout.html", {"request": request})
+
 
 # @app.get("/list-routers")
 # def list_routers():
